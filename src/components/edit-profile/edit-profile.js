26b26a6e268ef7e7b6ab32a255a7df94d21315c8
YogiDhingani <dhinganiyogi120720@gmail.com>
2022-11-12 00:13:14 +0530

    add profile page, working on calender

--- src/components/edit-profile/edit-profile.js
+++ src/components/edit-profile/edit-profile.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   DialogDropdownStyle,
   DialogInputStyle,
-  EditProfileDialogStyle
+  EditProfileDialogStyle,
+  GenderCardStyle,
+  GenderContainerStyle,
+  SaveChangesButtonStyle,
+  UploadContainerStyle
 } from '../../style-component/profile/edit-profile'
 import CrossIcon from '../../assets/images/close.svg'
 import PersonImage from '../../assets/images/person.png'
+import CONSTANT from '../../utils/constants'
+import LogoutIcon from '../../assets/images/log-out.svg'
 
 const EditProfile = ({ open, onClose }) => {
+  const [selectedGender, setSelectedGender] = useState(CONSTANT.gender[0])
   console.log('called')
   return (
     <EditProfileDialogStyle open={open} onClose={onClose}>
@@ -17,7 +24,7 @@ const EditProfile = ({ open, onClose }) => {
             <h3 className='dialogTitle'>Edit Profile</h3>
           </div>
 
-          <img src={CrossIcon} onClick={onClose} />
+          <img className='closeIcon' src={CrossIcon} onClick={onClose} />
         </div>
 
         <div className='dialogBody'>
@@ -42,8 +49,34 @@ const EditProfile = ({ open, onClose }) => {
               </DialogDropdownStyle>
             </div>
 
-            <div></div>
+            <GenderContainerStyle>
+              {CONSTANT.gender.map((gender, index) => {
+                return (
+                  <GenderCardStyle
+                    selected={selectedGender.value === gender.value}
+                    onClick={() => {
+                      setSelectedGender(gender)
+                    }}
+                    key={gender.value}
+                    first={index === 0}
+                    last={index === CONSTANT.gender.length - 1}
+                  >
+                    <span>{gender.label}</span>
+                  </GenderCardStyle>
+                )
+              })}
+            </GenderContainerStyle>
           </div>
+
+          <UploadContainerStyle>
+            <img className='uploadFile' src={LogoutIcon} alt='Upload' />
+
+            <span>Upload attachment</span>
+          </UploadContainerStyle>
+
+          <DialogInputStyle placeholder='Tell us about yourself' />
+
+          <SaveChangesButtonStyle>Save Changes</SaveChangesButtonStyle>
         </div>
       </div>
     </EditProfileDialogStyle>
--- src/style-component/profile/edit-profile.js
+++ src/style-component/profile/edit-profile.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 import Dialog from '@mui/material/Dialog'
 import {
+  Button,
   DropdownStyle,
   FlexAlignCenter,
+  FlexCenter,
   FlexJustifyCenter,
   FlexJustifySpaceBetween,
   InputStyle
 } from '../general'
-import theme from '../../utils/variables'
+import theme, { UNIVERSITY_COLOR } from '../../utils/variables'
 
 export const EditProfileDialogStyle = styled(Dialog)`
   .MuiPaper-root {
@@ -17,7 +19,7 @@ export const EditProfileDialogStyle = styled(Dialog)`
 
   .dialog {
     width: 70vw;
-    padding: 30px;
+    padding: 30px 30px 60px;
 
     .dialogHeader {
       ${FlexJustifySpaceBetween}
@@ -35,6 +37,10 @@ export const EditProfileDialogStyle = styled(Dialog)`
         font-size: 20px;
         line-height: 30px;
         color: ${theme.lightTheme.secondary.font};
+      }
+
+      .closeIcon {
+        cursor: pointer;
       }
     }
 
@@ -69,6 +75,7 @@ export const DialogInputStyle = styled.input`
   ${InputStyle}
   width: 100%;
   border: 1px solid ${theme.lightTheme.ternory.font};
+  height: auto;
 
   border-radius: 15px;
   font-family: 'Poppins';
@@ -78,6 +85,15 @@ export const DialogInputStyle = styled.input`
   line-height: 33px;
   color: ${theme.lightTheme.primary.textcolor};
   padding: 10px 18px;
+
+  &::placeholder {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 27px;
+    color: ${theme.lightTheme.quillGrey};
+  }
 `
 
 export const DialogDropdownStyle = styled.select`
@@ -93,4 +109,90 @@ export const DialogDropdownStyle = styled.select`
   line-height: 33px;
   color: ${theme.lightTheme.primary.textcolor};
   padding: 10px 18px;
+`
+
+export const GenderCardStyle = styled.div`
+  ${FlexCenter}
+  background-color: ${theme.lightTheme.seashell};
+  flex-grow: 1;
+  min-height: 55px;
+  cursor: pointer;
+  ${({ first }) => {
+    if (first) {
+      return {
+        borderRadius: '12px 0 0 12px'
+      }
+    }
+  }}
+  ${({ last }) => {
+    if (last) {
+      return {
+        borderRadius: '0 12px 12px 0'
+      }
+    }
+  }}
+    ${({ selected }) => {
+    if (selected) {
+      return {
+        background: UNIVERSITY_COLOR.primary
+      }
+    }
+  }}
+    span {
+    color: ${theme.lightTheme.ternory.font};
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+
+    ${({ selected }) => {
+      if (selected) {
+        return {
+          color: theme.lightTheme.black
+        }
+      }
+    }};
+  }
+`
+
+export const GenderContainerStyle = styled.div`
+  display: flex;
+  border-radius: 12px;
+  width: 100%;
+`
+
+export const UploadContainerStyle = styled.div`
+  border: 1px solid ${theme.lightTheme.ternory.font};
+  width: 100%;
+  min-height: 55px;
+  border-radius: 15px;
+  position: relative;
+  ${FlexAlignCenter}
+  padding: 0 20px;
+
+  .uploadFile {
+    position: absolute;
+    right: 20px;
+    top: 15px;
+    transform: rotate(-90deg);
+  }
+
+  span {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 27px;
+    color: ${theme.lightTheme.quillGrey};
+  }
+`
+
+export const SaveChangesButtonStyle = styled.button`
+  ${Button}
+  width: 100%;
+  margin: 0;
+  box-shadow: none;
+  background: ${UNIVERSITY_COLOR.primary};
+  color: ${theme.lightTheme.black};
 `
